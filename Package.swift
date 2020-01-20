ff33bd2c342ed260fd043912a5561d6b97a40ac3
Mohammad Porooshani <porooshani@gmail.com>
2020-01-20 08:54:46 +0330

    Updated to swift 5 in package manifest
    Signed-off-by: Mohammad Porooshani <porooshani@gmail.com>

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
@@ -15,5 +15,6 @@ let package = Package(
             dependencies: ["CocoaAsyncSocket"],
             path: "iOS/Source"
         )
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
